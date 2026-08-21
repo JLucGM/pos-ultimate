19a3a7bdbeea02fcf4dcea4a23810834728601cd
eddy_vill <66701791+eddyvill@users.noreply.github.com>
2026-08-21 08:30:26 -0400

    fix: prevenir View [] not found en module_form_part al renderizar sell.create

--- resources/views/layouts/partials/module_form_part.blade.php
+++ resources/views/layouts/partials/module_form_part.blade.php
@@ -1,8 +1,8 @@
 @if(!empty($module_form_parts))
   @foreach($module_form_parts as $key => $value)
-    @if(!empty($value['template_path']))
+    @if(!empty($value['template_path']) && is_string($value['template_path']) && View::exists($value['template_path']))
       @php
-        $template_data = $value['template_data'] ?: [];
+        $template_data = !empty($value['template_data']) ? $value['template_data'] : [];
       @endphp
       @include($value['template_path'], $template_data)
     @endif
